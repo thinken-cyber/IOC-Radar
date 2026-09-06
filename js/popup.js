a270f9ee1fb6044c49af346c1b31f92f8c6677c2
thinken-cyber <thinken-cyber@users.noreply.github.com>
2026-09-06 23:11:16 +0700

    Fix popup hash renderer import

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,4 @@
-import { detectIOC, typeLabel, IOC_TYPES } from '../lib/detect.js';
+import { detectIOC, isHash, typeLabel, IOC_TYPES } from '../lib/detect.js';
 import { pruneCache } from '../lib/cache.js';
 import { VERDICT_LABEL_VI } from '../lib/aggregate.js';
 
